8c1175b72221b3089155368023bc6e0189ea7062
Anderson-Junior <ander.lemos.jr@gmail.com>
2023-11-26 19:47:28 -0300

    Implementando testes unitarios

---
+++ UtmBuilder.Core.Tests/ValueObjects/CampaignTests.cs
@@ -0,0 +1,35 @@
+﻿using UtlBuilder.Core.ValueObjects;
+using UtlBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.Tests.ValueObjects
+{
+    [TestClass]
+    public class CampaignTests
+    {
+        [TestMethod]
+        [DataRow("", "", "", true)]
+        [DataRow("src", "", "", true)]
+        [DataRow("src", "med", "", true)]
+        [DataRow("src", "med", "nam", false)]
+        public void Campaign(string source, string medium, string name, bool expectExcpetion) 
+        { 
+            if(expectExcpetion)
+            {
+                try
+                {
+                    new Campaign(source, medium, name);
+                    Assert.Fail();
+                }
+                catch(InvalidCampaignException)
+                {
+                    Assert.IsTrue(true);
+                }
+            }
+            else
+            {
+                new Campaign(source, medium, name);
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
---
+++ UtmBuilder.Core.Tests/ValueObjects/UtlTests.cs
@@ -0,0 +1,51 @@
+﻿using UtlBuilder.Core.ValueObjects;
+using UtlBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.Tests.ValueObjects
+{
+    [TestClass]
+    public class UtlTests
+    {
+        private const string InvalidUrl = "banana";
+        private const string ValidUrl = "https://balta.io";
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidUrlException))]
+        public void ShouldReturnExceptionWhenUrlIsInvalid()
+        {
+            new Url(InvalidUrl);
+        }
+
+        [TestMethod]
+        public void ShouldNotReturnExceptionWhenUrlIsValid()
+        {
+            new Url(ValidUrl);
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        [DataRow(" ", true)]
+        [DataRow("http", true)]
+        [DataRow("banana", true)]
+        [DataRow("https://balta.io", false)]
+        public void TestUrl(string link, bool expectException)
+        {
+            if (expectException)
+            {
+                try
+                {
+                    new Url(link);
+                    Assert.Fail();
+
+                }catch(InvalidUrlException) 
+                {
+                    Assert.IsTrue(true);
+                }
+            }
+            else
+            {
+                new Url(link);
+            }
+        }
+    }
+}
